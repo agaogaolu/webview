b2779258b830b8f690d4dcac3da5bae9d7372a64
agaogao <agaogaolu@qq.com>
2024-08-16 12:08:50 +0800

    first commit

---
+++ src/App.vue
@@ -0,0 +1,107 @@
+<template>
+  <el-container style="height: 100vh;">
+    <el-aside width="200px" style="background-color: #545c64;">
+      <el-menu
+        :default-active="activeMenu"
+        class="el-menu-vertical-demo"
+        background-color="#545c64"
+        text-color="#fff"
+        active-text-color="#ffd04b"
+        style="display: flex; flex-direction: column; justify-content: center; align-items: center; height: 100%;"
+      >
+        <el-menu-item index="1" @click="selectMenu('upload')">
+          <el-icon><upload /></el-icon>
+          <span slot="title">上传图片功能</span>
+        </el-menu-item>
+        <el-menu-item index="2" @click="selectMenu('feature2')">
+          <el-icon><view /></el-icon>
+          <span slot="title">功能二</span>
+        </el-menu-item>
+        <el-menu-item index="3" @click="selectMenu('feature3')">
+          <el-icon><setting /></el-icon>
+          <span slot="title">功能三</span>
+        </el-menu-item>
+      </el-menu>
+    </el-aside>
+
+    <el-container>
+      <el-main style="display: flex; justify-content: center; align-items: center;">
+        <component :is="currentComponent"></component>
+      </el-main>
+    </el-container>
+  </el-container>
+</template>
+
+<script>
+import UploadImage from './components/UploadImage.vue';
+import FeatureTwo from './components/FeatureTwo.vue';
+import FeatureThree from './components/FeatureThree.vue';
+import { Upload, View, Setting } from '@element-plus/icons-vue';
+
+export default {
+  components: {
+    UploadImage,
+    FeatureTwo,
+    FeatureThree,
+    upload: Upload,
+    view: View,
+    setting: Setting,
+  },
+  data() {
+    return {
+      activeMenu: '1',
+      currentFeature: 'upload',
+    };
+  },
+  computed: {
+    currentComponent() {
+      switch (this.currentFeature) {
+        case 'upload':
+          return UploadImage;
+        case 'feature2':
+          return FeatureTwo;
+        case 'feature3':
+          return FeatureThree;
+        default:
+          return null;
+      }
+    },
+  },
+  methods: {
+    selectMenu(feature) {
+      this.currentFeature = feature;
+      this.activeMenu = feature === 'upload' ? '1' : feature === 'feature2' ? '2' : '3';
+    },
+  },
+};
+</script>
+
+<style scoped>
+* {
+  padding: 0;
+  margin: 0;
+}
+.el-aside {
+  display: flex;
+  background-color: #545c64;
+  color: #fff;
+  padding: 0;
+  align-items: center;
+}
+
+.el-menu-vertical-demo {
+  border-right: none;
+}
+
+.el-menu-item {
+
+  padding: 0 20px;
+}
+
+.el-menu-item .el-icon {
+  margin-right: 10px;
+}
+.el-main{
+  
+}
+</style>
---
+++ src/components/FeatureThree.vue
@@ -0,0 +1,87 @@
+<template>
+    <div>
+      <el-upload
+        class="upload-demo"
+        action=""
+        :show-file-list="false"
+        :before-upload="handleBeforeUpload"
+      >
+        <el-button size="small" type="primary">点击上传图片</el-button>
+      </el-upload>
+      <div v-if="imagePreview">
+        <h3>当前选择的图片预览：</h3>
+        <img :src="imagePreview" alt="Selected Image" style="max-width: 100%; height: auto;" />
+      </div>
+      <el-button v-if="imagePreview" type="success" @click="uploadImage">上传并处理图片</el-button>
+      <div v-if="processedImage">
+        <h3>处理后的图片：</h3>
+        <img :src="processedImage" alt="Processed Image" style="max-width: 100%; height: auto;" />
+      </div>
+      <div v-if="responseData">
+        <h3>SeedCount: {{ responseData.seed_count }}</h3>
+      </div>
+    </div>
+    
+  </template>
+  
+  <script>
+  // 导入封装的请求方法
+  import { post } from '@/services/api';
+  
+  export default {
+    data() {
+      return {
+        selectedFile: null,
+        imagePreview: null,
+        processedImage: null,
+        responseData:null
+      };
+    },
+    methods: {
+      handleBeforeUpload(file) {
+        this.selectedFile = file;
+  
+        const reader = new FileReader();
+        reader.readAsDataURL(file);
+  
+        reader.onload = () => {
+          this.imagePreview = reader.result;
+        };
+  
+        return false; // 阻止自动上传
+      },
+      async uploadImage() {
+        if (!this.selectedFile) return;
+  
+        const reader = new FileReader();
+        reader.readAsDataURL(this.selectedFile);
+  
+        reader.onload = async () => {
+            console.log(reader)
+          const base64Image = reader.result;
+          const cleanedBase64String = base64Image.replace(/^data:image\/\w+;base64,/, '');
+  
+          try {
+            // 使用封装的 POST 方法
+            const response = await post('/api/seedcount', { image: cleanedBase64String });
+            // this.processedImage = response.processedImage; // 假设返回的处理结果在 `processedImage` 字段中
+            // response.confidence = (response.confidence * 100).toFixed(2) + '%';
+            this.processedImage = "data:image/jpeg;base64," + response.segmented_image
+            this.responseData = response
+            console.log(response)
+          } catch (error) {
+            console.error('上传图片失败:', error);
+          }
+        };
+      },
+    },
+  };
+  </script>
+  
+  <style>
+  .upload-demo {
+    display: block;
+    margin-bottom: 20px;
+  }
+  </style>
+  
